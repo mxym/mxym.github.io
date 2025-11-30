b0595f992457868f9160ccdfa5f645a7f5747719
mxym <2861703402@qq.com>
2025-12-01 00:40:09 +0800

    成功建好

--- src/config.ts
+++ src/config.ts
@@ -6,13 +6,12 @@ import type {
     SiteConfig,
 } from "./types/config";
 import { LinkPreset } from "./types/config";
-
 export const siteConfig: SiteConfig = {
-    title: "DearNikki",
-    subtitle: "A place to write on.",
-    lang: "en", // Language code, e.g. 'en', 'zh_CN', 'ja', etc.
+    title: "mxymの小窝",
+    subtitle: "萌新一枚喵",
+    lang: "zh_CN", // Language code, e.g. 'en', 'zh_CN', 'ja', etc.
     themeColor: {
-        hue: 250, // Default hue for the theme color, from 0 to 360. e.g. red: 0, teal: 200, cyan: 250, pink: 345
+        hue: 200, // Default hue for the theme color, from 0 to 360. e.g. red: 0, teal: 200, cyan: 250, pink: 345
         fixed: false, // Hide the theme color picker for visitors
     },
     banner: {
@@ -49,37 +48,26 @@ export const navBarConfig: NavBarConfig = {
         LinkPreset.About,
         {
             name: "GitHub",
-            url: "https://github.com/Rabbit0w0/dear-nikki", // Internal links should not include the base path, as it is automatically added
+            url: "https://github.com/mxym/", // Internal links should not include the base path, as it is automatically added
             external: true, // Show an external link icon and will open in a new tab
         },
     ],
     logo: {
         image: true, // Display an image as logo on the navbar
-        src: "assets/images/demo-logo.png", // Relative to the /src directory. Relative to the /public directory if it starts with '/'
+        src: "assets/images/logo.jpg", // Relative to the /src directory. Relative to the /public directory if it starts with '/'
     },
 };
 
 export const profileConfig: ProfileConfig = {
-    avatar: "assets/images/demo-avatar.png", // Relative to the /src directory. Relative to the /public directory if it starts with '/'
-    name: "Dearest You",
-    bio: "Sometimes I write things that matter. Sometimes they just feel nice to come out.",
+    avatar: "assets/images/demo-avatar.jpg", // Relative to the /src directory. Relative to the /public directory if it starts with '/'
+    name: "mxym",
+    bio: "萌新一枚",
     links: [
-        {
-            name: "Twitter",
-            icon: "fa6-brands:twitter", // Visit https://icones.js.org/ for icon codes
-            // You will need to install the corresponding icon set if it's not already included
-            // `pnpm add @iconify-json/<icon-set-name>`
-            url: "https://twitter.com",
-        },
-        {
-            name: "Steam",
-            icon: "fa6-brands:steam",
-            url: "https://store.steampowered.com",
-        },
+
         {
             name: "GitHub",
             icon: "fa6-brands:github",
-            url: "https://github.com/",
+            url: "https://github.com/mxym/",
         },
     ],
 };
